ff38c28b1cc6f628cbc98d163c7bccdf2a32420e
Yifeng Tang <ytang@mathworks.com>
2021-09-24 21:42:35 -0400

    v7 add teammates to action table

--- Scripts/init_GameActions.m
+++ Scripts/init_GameActions.m
@@ -2,35 +2,21 @@
 
 clear GameActions
 
-% % 10 table fields
-% i = 0; 
-% varNames = {};  
-% varTypes = {};
-% i=i+1; varNames{i} = 'Team';    varTypes{i} = 'string';
-% i=i+1; varNames{i} = 'Player';  varTypes{i} = 'string';
-% i=i+1; varNames{i} = 'Action';  varTypes{i} = 'string';
-% i=i+1; varNames{i} = 'Shoot';   varTypes{i} = 'string';
-% i=i+1; varNames{i} = 'Result';  varTypes{i} = 'string';
-% i=i+1; varNames{i} = 'Pass';    varTypes{i} = 'string';
-% i=i+1; varNames{i} = 'Rebound'; varTypes{i} = 'string';
-% i=i+1; varNames{i} = 'Defense'; varTypes{i} = 'string';
-% i=i+1; varNames{i} = 'Other';   varTypes{i} = 'string';
-% i=i+1; varNames{i} = 'Notes';   varTypes{i} = 'string';
-% sz = [0 i];
-% % new empty table
-% GameActions = table('Size',sz,'VariableTypes',varTypes,'VariableNames',varNames);
-
-% 6 table fields
 i = 0; 
 varNames = {};  
 varTypes = {};
-i=i+1; varNames{i} = 'Team';    varTypes{i} = 'string';
+i=i+1; varNames{i} = 'id';      varTypes{i} = 'double';
 i=i+1; varNames{i} = 'Player';  varTypes{i} = 'string';
 i=i+1; varNames{i} = 'Action';  varTypes{i} = 'string';
 i=i+1; varNames{i} = 'Detail1'; varTypes{i} = 'string';
 i=i+1; varNames{i} = 'Detail2'; varTypes{i} = 'string';
 i=i+1; varNames{i} = 'Notes';   varTypes{i} = 'string';
-i=i+1; varNames{i} = 'id';      varTypes{i} = 'double';
+i=i+1; varNames{i} = 'Team';    varTypes{i} = 'string';
+i=i+1; varNames{i} = 'Teammate1';   varTypes{i} = 'string';
+i=i+1; varNames{i} = 'Teammate2';   varTypes{i} = 'string';
+i=i+1; varNames{i} = 'Teammate3';   varTypes{i} = 'string';
+i=i+1; varNames{i} = 'Teammate4';   varTypes{i} = 'string';
+i=i+1; varNames{i} = 'Teammate5';   varTypes{i} = 'string';
 sz = [0 i];
 % new empty table
 GameActions = table('Size',sz,'VariableTypes',varTypes,'VariableNames',varNames);
